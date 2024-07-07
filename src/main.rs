0224c35d92f8d24816d93d27feca06f54d90c6a6
Akiomi Kamakura <akiomik@gmail.com>
2024-07-07 17:12:48 +0900

    Use fit-rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use fit_rust::protocol::message_type::MessageType;
+use fit_rust::protocol::FitMessage;
+use fit_rust::Fit;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read("files/broken.fit").unwrap();
+
+    let fit: Fit = Fit::read(file).unwrap();
+    for data in &fit.data {
+        match data {
+            FitMessage::Definition(msg) => {
+                println!("Definition: {:?}", msg.data);
+            }
+            FitMessage::Data(msg) if msg.data.message_type == MessageType::FileId => {
+                println!("FileId data type: {:?}", msg.data);
+            }
+            FitMessage::Data(msg) => {
+                // Other data type is here
+                println!("Other: {:?}", msg.data);
+            }
+        }
+    }
 }
