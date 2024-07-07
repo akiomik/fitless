af67922e09dbff45770022eaddbb8f0aacb77d1a
Akiomi Kamakura <akiomik@gmail.com>
2024-07-07 18:01:44 +0900

    Use fitparser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-use fit::Fit;
-use std::path::PathBuf;
+use std::error::Error;
+use std::fs::File;
 
-fn main() {
-    let filepath = PathBuf::from("files/broken.fit");
-    let f = Fit::new(&filepath);
-    for m in f {
-        println!("Read a message of type {:?}", m);
-    }
+use fitparser::de::{from_reader_with_options, DecodeOption};
+use fitparser::ErrorKind;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!(
+        "Parsing FIT files using Profile version: {}",
+        fitparser::profile::VERSION
+    );
+    let mut fp = File::open("files/broken-fixed.fit")?;
+
+    let opts = [
+        DecodeOption::SkipHeaderCrcValidation,
+        DecodeOption::SkipDataCrcValidation,
+        DecodeOption::DropUnknownFields,
+        DecodeOption::DropUnknownMessages,
+    ]
+    .iter()
+    .map(|o| *o)
+    .collect();
+
+    match from_reader_with_options(&mut fp, &opts) {
+        Ok(records) => {
+            for data in records {
+                println!("{:#?}", data);
+            }
+        }
+        Err(e) => match *e {
+            ErrorKind::ParseError(_, t) => println!("{}", t.description()),
+            _ => println!("error!: {:?}", e),
+        },
+    };
+
+    Ok(())
 }
